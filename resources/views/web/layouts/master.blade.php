7dbe9b24ea1d6b5e3ed90455c076241c3f5a0486
Ikramul Hasan <hasanikramul926@gmail.com>
2025-02-10 20:56:50 +0600

    navbar add companies name rename Company

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -211,7 +211,7 @@
                                     {{ Request::is('about*') ? 'current' : '' }}
                                     {{ Request::is('faqs*') ? 'current' : '' }}
                                     {{ Request::is('contact*') ? 'current' : '' }}
-                                    "><a href="">Companies</a>
+                                    "><a href="">Company</a>
                                         <ul>
                                             @if(isset($page_about))
                                             <li class="{{ Request::is('about*') ? 'current' : '' }}"><a href="{{ route('about') }}">{{ $page_about->title }}</a></li>
@@ -331,7 +331,7 @@
                                     @endphp
 
                                     @if(isset($page_about) || isset($page_faqs) ||isset($page_contact) )
-                                    <li class="dropdown {{ Request::is('service*') ? 'current' : '' }}"><a href="">Companies</a>
+                                    <li class="dropdown {{ Request::is('service*') ? 'current' : '' }}"><a href="">Company</a>
                                         <ul>
                                             @if(isset($page_about))
                                             <li class="{{ Request::is('about*') ? 'current' : '' }}"><a href="{{ route('about') }}">{{ $page_about->title }}</a></li>
