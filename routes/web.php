308945cb4dabae621953472acb94b13245cb31c5
Ikramul Hasan <hasanikramul926@gmail.com>
2025-02-27 18:29:36 +0600

    middlewaredsd

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::namespace('Web')->group(function () {
+Route::middleware(['XSS'])->namespace('Web')->group(function () {
 
     // Home Route
     Route::get('/', 'HomeController@index')->name('home');
