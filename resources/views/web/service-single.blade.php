9ff2a15e2a20d332482c77b46047c0954dcb3602
ikramulhasan1 <hasanikramul926@gmail.com>
2025-03-05 20:22:21 +0600

    font color and formate changedf

--- resources/views/web/service-single.blade.php
+++ resources/views/web/service-single.blade.php
@@ -234,7 +234,7 @@ $header = \App\Models\PageSetup::page('services');
 @if(isset($service))
 <!--Sidebar Page Container-->
 <div style="background-color: #f7fff9" class="sidebar-page-container">
-    <div class="mx-5">
+    <div class="mx-4">
         <div class="row clearfix mb-5">
             <!--Sidebar Side-->
             <div class="sidebar-side col-lg-4 col-md-12 col-sm-12">
