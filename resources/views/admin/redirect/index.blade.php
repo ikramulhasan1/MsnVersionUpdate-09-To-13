0fbe7a6e1f6114ddeb2e69c6d0a5211e0e568a5e
ikramulhasan1 <hasanikramul926@gmail.com>
2025-03-07 16:57:34 +0600

    redirect urlds

--- resources/views/admin/redirect/index.blade.php
+++ resources/views/admin/redirect/index.blade.php
@@ -71,9 +71,28 @@
                                         <i class="far fa-edit"></i>
                                     </a>
 
-                                    {{-- <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal-{{ $row->id }}">
+                                    <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal-{{ $row->id }}">
                                         <i class="fas fa-trash-alt"></i>
-                                    </button> --}}
+                                    </button>
+
+                                    <div class="modal fade" id="deleteModal-{{ $row->id }}" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-sm">
+                                        <form action="{{ route('admin.redirects.destroy', [$row->id]) }}" method="post" class="delete-form">
+                                        @csrf
+                                        @method('DELETE')
+                                            <div class="modal-content">
+                                                <div class="modal-body text-center">
+                                                    <h3>{{ __('dashboard.are_you_sure') }}</h3>
+                                                    <p>{{ __('dashboard.delete_warning') }}</p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="submit" class="btn btn-danger">{{ __('dashboard.confirm') }}</button>
+                                                    <button type="button" class="btn btn-light" data-dismiss="modal">{{ __('dashboard.close') }}</button>
+                                                </div>
+                                            </div><!-- /.modal-content -->
+                                        </form>
+                                        </div><!-- /.modal-dialog -->
+                                    </div>
                                     <!-- Include Delete modal -->
                                     {{-- @include('admin.inc.delete') --}}
                                 </td>
