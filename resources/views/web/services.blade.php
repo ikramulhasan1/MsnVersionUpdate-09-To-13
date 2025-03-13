11619825686c89e3b0c9b2e739248225e70717e4
ikramulhasan1 <hasanikramul926@gmail.com>
2025-03-13 16:44:38 +0600

    services short desddssdsdsdd

--- resources/views/web/services.blade.php
+++ resources/views/web/services.blade.php
@@ -126,7 +126,7 @@ $section_services = \App\Models\Section::section('services');
                             <div class="overlay-box"><a href="{{ route('service.single', $service->slug) }}">{{ __('common.read_more') }}</a></div>
                         </div>
                         <div class="lower-content">
-                            <h3><a href="{{ route('service.single', $service->slug) }}">{{ $service->title }}</a></h3>
+                            <h3><a href="{{ route('service.single', $service->slug) }}">{{ $service->short_title }}</a></h3>
                             <div class="text">{!! strip_tags(Str::words($service->short_desc, 20)) !!}</div>
                         </div>
                     </div>
