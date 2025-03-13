2fe9423e1a58433f42c288888c7e82cf17e7a1d2
ikramulhasan1 <hasanikramul926@gmail.com>
2025-03-13 21:34:15 +0600

    services short desdf

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -245,7 +245,7 @@ $section_services = \App\Models\Section::section('services');
 @endphp
 @if(count($services) > 0 && isset($section_services))
 <!-- Services Section -->
-<section class="services-section style-four">
+<section>
     <div class="container">
         <div class="row">
             <div class="col-12">
