4b53eb0b2a8e70ee4352e235246f6de117414e43
ikramulhasan1 <hasanikramul926@gmail.com>
2025-03-15 19:30:34 +0600

    sitemap editf

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,6 +1,8 @@
 {!! '<'.'?xml version="1.0" encoding="UTF-8"?'.'>' !!}
 
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+
   {{-- <url>
     <loc>{{ URL::to("/") }}</loc>
     <lastmod>2025-05-05</lastmod>
@@ -38,23 +40,18 @@
     <lastmod>{{ now()->format('Y-m-d') }}</lastmod>
   </url>
 
-</urlset>
-
-
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
-
-    @foreach ($articles as $article)
-        <url>
+  @foreach ($articles as $article)
+      <url>
         <loc>{{ route('blog.single',['slug'=>$article->slug]) }}</loc>
         <news:news>
             <news:publication>
-            <news:name>{{ env('APP_NAME') }}</news:name>
-            <news:language>en</news:language>
+                <news:name>{{ env('APP_NAME') }}</news:name>
+                <news:language>en</news:language>
             </news:publication>
             <news:publication_date>{{ date("Y-m-d", strtotime($article->created_at)) }}</news:publication_date>
             <news:title>{{ $article->title }}</news:title>
         </news:news>
-        </url>
-    @endforeach
+      </url>
+  @endforeach
+
 </urlset>
