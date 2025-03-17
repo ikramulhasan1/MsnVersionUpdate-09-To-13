2f81ce6020d53f4cd78132eb2a42ad5bc6d5c776
ikramulhasan1 <hasanikramul926@gmail.com>
2025-03-18 00:24:57 +0600

    link reeditgdfgffkjgddffdfdfdddsfdsf

--- resources/views/admin/service/create.blade.php
+++ resources/views/admin/service/create.blade.php
@@ -89,14 +89,14 @@
                         <div class="row">
                             <div class="form-group col">
                                 <label for="price">{{ __('dashboard.price') }} <span>* </span></label>
-                                <input type="number" class="form-control" name="price" id="price" value="100" required>
+                                <input type="number" class="form-control" name="price" id="price" value="100 {{ old('price') }}" required>
                                 <div class="invalid-feedback">
                                     {{ __('dashboard.please_provide') }} {{ __('dashboard.price') }}
                                 </div>
                             </div>
                             <div class="form-group col">
                                 <label for="starting_price">{{ __('dashboard.starting_price') }} <span>*</span></label>
-                                <input type="number" class="form-control" name="starting_price" id="starting_price" value="100" required>
+                                <input type="number" class="form-control" name="starting_price" id="starting_price" value="100 {{ old('starting_price') }}" required>
     
                                 <div class="invalid-feedback">
                                     {{ __('dashboard.please_provide') }} {{ __('dashboard.starting_price') }}
@@ -104,7 +104,7 @@
                             </div>
                             <div class="form-group col">
                                 <label for="review_count">{{ __('dashboard.review_count') }} <span>*</span></label>
-                                <input type="number" class="form-control" name="review_count" id="review_count" value="150" required>
+                                <input type="number" class="form-control" name="review_count" id="review_count" value="150 {{ old('review_count') }}" required>
     
                                 <div class="invalid-feedback">
                                     {{ __('dashboard.please_provide') }} {{ __('dashboard.review_count') }}
@@ -112,7 +112,7 @@
                             </div>
                             <div class="form-group col">
                                 <label for="priceCurrency">{{ __('dashboard.priceCurrency') }} <span>*</span></label>
-                                <input type="text" class="form-control" name="priceCurrency" id="priceCurrency" value="USD" required>
+                                <input type="text" class="form-control" name="priceCurrency" id="priceCurrency" value="USD {{ old('priceCurrency') }}" required>
     
                                 <div class="invalid-feedback">
                                     {{ __('dashboard.please_provide') }} {{ __('dashboard.priceCurrency') }}
@@ -120,7 +120,7 @@
                             </div>
                             <div class="form-group col">
                                 <label for="average_rating">{{ __('dashboard.average_rating') }} <span>*</span></label>
-                                <input type="text" class="form-control" name="average_rating" id="average_rating" value="4.9" required>
+                                <input type="text" class="form-control" name="average_rating" id="average_rating" value="4.9 {{ old('average_rating') }}" required>
     
                                 <div class="invalid-feedback">
                                     {{ __('dashboard.please_provide') }} {{ __('dashboard.average_rating') }}
