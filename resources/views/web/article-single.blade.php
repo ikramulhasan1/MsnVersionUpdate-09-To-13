b24c3a5ef1593689cff215a7db119e2e36864580
ikramulhasan1 <hasanikramul926@gmail.com>
2025-03-17 06:16:09 +0600

    image webPssfsddddddd

--- resources/views/web/article-single.blade.php
+++ resources/views/web/article-single.blade.php
@@ -318,13 +318,9 @@ $header = \App\Models\PageSetup::page('blog');
                                     @endphp
 
                                     @if(isset($page_quote))
-                                     <!-- Service Sell Title -->
-                                     <div class="service-title">
-                                        <h3>Service Sell</h3> <!-- Title text -->
-                                    </div>
                                         <div class="circle-container">
-                                           
-
+                                            <!-- Service Sell Title -->
+                                            
                                             <!-- Get A Quote Button -->
                                             <a href="{{ route('get-quote') }}" target="_blank" class="circle-button">
                                                 <img src="https://cdn-icons-png.flaticon.com/128/18572/18572275.png" alt="Get A Quote">
