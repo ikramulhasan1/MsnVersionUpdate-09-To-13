1d0bc208c23ae156624a3ee5052928ed0087241c
ikramulhasan1 <hasanikramul926@gmail.com>
2025-03-18 23:26:44 +0600

    blog cofgdsdsjgggfljfffgff

--- app/Http/Controllers/Web/ArticleController.php
+++ app/Http/Controllers/Web/ArticleController.php
@@ -102,7 +102,7 @@ class ArticleController extends Controller
 if (!empty($data['article']->service) && !empty($data['article']->service_title) && !empty($data['article']->service_desc)) {
     // Replace the <li> elements with the ✅ emoji
     $description = $data['article']->service_desc;
-    $description = preg_replace('/<li>(.*?)<\/li>/i', '✅ $1', $description);
+    $description = preg_replace('/<li>(.*?)<\/li>/i', '<p>✅ $1</p>', $description);
 
     $packageHtml = "<div class='service-package' style='
     background: #1E2A38; 
