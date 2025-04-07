5098c5112129b95c154e9f6b80ed662217fe53ad
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-08 01:46:28 +0600

    new sub service addhhggggggghetwegh

--- resources/views/web/service-single.blade.php
+++ resources/views/web/service-single.blade.php
@@ -428,8 +428,8 @@ $header = \App\Models\PageSetup::page('services');
                 @if (!empty($service->subservices))
                 @foreach ($service->subservices as $key => $item)
                     <div class="col-lg-4 col-md-6 col-sm-12">
-                        <div class="card h-100">
-                            <img  src="{{ asset('uploads/service/'.$item->image_path) }}" alt="{{ $item->title }}" class="card-img-top">
+                        <div class="card">
+                            <img  src="{{ asset('uploads/service/'.$item->image_path) }}" alt="{{ $item->title }}" class="img-fluid card-img-top">
                             <div class="card-body">
                                 <h5 class="card-title"><a href="{{ route('service.related-single',$item->slug) }}">{{ $item->title }}</a></h5>
                                 <p class="card-text">{!! Str::words($item->description, 10, '...') !!}</p>
