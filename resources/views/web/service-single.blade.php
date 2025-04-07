73a8a687999811cd89d9e02496d271e90675c1f7
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-08 02:11:06 +0600

    new sub service addhhggggggghetwegtegthuythggghygg

--- resources/views/web/service-single.blade.php
+++ resources/views/web/service-single.blade.php
@@ -425,6 +425,7 @@ $header = \App\Models\PageSetup::page('services');
 
         
             <div class="row g-1">
+                <h3>Related Services</h3>
                 @if (!empty($service->subservices))
                 @foreach ($service->subservices as $key => $item)
                     <div class="col-lg-3 col-md-4 col-sm-6 col-12 col-xs-12 mb-3">
