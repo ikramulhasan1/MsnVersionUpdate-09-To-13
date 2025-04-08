5e4452bc43ca449e3db5baf85c50520e63e8c72b
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-09 03:46:40 +0600

    jjgffdffffffdfwwddedfdsddf

--- resources/views/admin/technology/create.blade.php
+++ resources/views/admin/technology/create.blade.php
@@ -62,14 +62,6 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="short_desc">{{ __('dashboard.meta_description') }} <span>*</span></label>
-                            <textarea class="form-control" name="short_desc" id="editor" rows="4" required>{{ old('short_desc') }}</textarea>
-
-                            <div class="invalid-feedback">
-                                {{ __('dashboard.please_provide') }} {{ __('dashboard.meta_description') }}
-                            </div>
-                        </div>
 
                         <div class="form-group">
                             <label for="description">{{ __('dashboard.description') }} <span>*</span></label>
@@ -77,6 +69,15 @@
 
                             <div class="invalid-feedback">
                                 {{ __('dashboard.please_provide') }} {{ __('dashboard.description') }}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="short_desc">{{ __('dashboard.meta_description') }} <span>*</span></label>
+                            <textarea class="form-control" name="short_desc" id="editor" rows="4" required>{{ old('short_desc') }}</textarea>
+
+                            <div class="invalid-feedback">
+                                {{ __('dashboard.please_provide') }} {{ __('dashboard.meta_description') }}
                             </div>
                         </div>
                         <div class="form-group">
