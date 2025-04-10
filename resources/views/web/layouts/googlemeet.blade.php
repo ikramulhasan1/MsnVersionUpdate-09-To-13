78896d87be7f7ce975d4e54e37e8c44a4685dc35
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-10 19:57:54 +0600

    hgfkdddedfdsdwdsas

--- resources/views/web/layouts/googlemeet.blade.php
+++ resources/views/web/layouts/googlemeet.blade.php
@@ -83,7 +83,7 @@
           </div> --}}
           
           <!-- Modal -->
-          <div id="googleMeetModal" class="fixed inset-0 bg-black bg-opacity-50 hidden items-center justify-center">
+          <div id="googleMeetModal" class="fixed inset-0 bg-black bg-opacity-50 hidden items-center justify-center z-50">
             <div class="bg-white w-full max-w-lg p-6 rounded-xl shadow-xl relative animate-fadeIn">
           
               <!-- Close Button -->
