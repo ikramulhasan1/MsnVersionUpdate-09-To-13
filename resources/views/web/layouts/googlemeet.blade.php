82db8f017648ea7c123218f1789ab33ebad9db30
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-12 00:54:32 +0600

    HJfdfsssssdadsssds

--- resources/views/web/layouts/googlemeet.blade.php
+++ resources/views/web/layouts/googlemeet.blade.php
@@ -104,18 +104,21 @@
     }
     .logo-container {
   position: relative;
-  width: 124px;  /* fixed width based on the largest image */
-  height: 40px;  /* fixed height */
+  width: 124px;
+  height: 40px;
   display: inline-block;
+  overflow: hidden;
 }
+
 
 .meeting-logo {
   position: absolute;
-  top: 0;
+  top: 50%; /* vertical centering start */
   left: 0;
   width: 100%;
-  height: 100%;
+  height: auto;
   object-fit: contain;
+  transform: translateY(-50%); /* perfect vertical alignment */
   opacity: 0;
   transition: opacity 1s ease-in-out;
   pointer-events: none;
