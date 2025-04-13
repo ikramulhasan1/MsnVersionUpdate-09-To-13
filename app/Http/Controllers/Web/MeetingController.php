826fa518efe8b516e80cd227d6ad5c86ea0f18bf
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-13 23:00:56 +0600

    example.envdssddssfsdddsddssssfdsdsssddweeddddddseeswrdsdrddde

--- app/Http/Controllers/Web/MeetingController.php
+++ app/Http/Controllers/Web/MeetingController.php
@@ -20,7 +20,6 @@ class MeetingController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
          // Validate the incoming data
          $validated = $request->validate([
             'name' => 'required|string|max:255',
