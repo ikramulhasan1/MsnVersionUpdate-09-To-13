820bbaecf35a8ecf7247da3595b2b754198d3c98
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-14 21:47:42 +0600

    lgdssdsdfd4dssfsdsddd

--- resources/views/web/article-single.blade.php
+++ resources/views/web/article-single.blade.php
@@ -458,15 +458,14 @@ $header = \App\Models\PageSetup::page('blog');
                                                     <img src="https://cdn-icons-png.flaticon.com/128/732/732200.png" alt="Email">
                                                 </a>
                                             </div>
-                                            
+                                            <div style="z-index: 99 !important;">
+                                                @include('web.layouts.googlemeet')
+                                            </div>
                                         </div>
                                     @elseif(isset($page_contact))
                                         <a href="{{ route('contact') }}" class="theme-btn btn-style-four mt-3">{{ __('common.get_start') }}</a>
                                     @endif
 
-                                </div>
-                                <div class=" ">
-                                    @include('web.layouts.googlemeet')
                                 </div>
                             </div>
                         </div>
