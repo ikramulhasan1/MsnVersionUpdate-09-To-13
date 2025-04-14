07fd553d34b09eac8081f968db45b1e15f6be985
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-14 20:17:00 +0600

    lgdssdsdfdssdddddsdeddrfdfddfdddg

--- resources/views/admin/meeting/index.blade.php
+++ resources/views/admin/meeting/index.blade.php
@@ -4,6 +4,7 @@
 
 <!-- jQuery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 <!-- Bootstrap Toggle CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap4-toggle@3.6.1/css/bootstrap4-toggle.min.css" rel="stylesheet">
 
@@ -16,88 +17,88 @@
 <!-- Toastr JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
-    <!-- start page title -->
-    <!-- Include page breadcrumb -->
-    @include('admin.inc.breadcrumb')
-    <!-- end page title --> 
-    <div class="row">
-        <div class="col-12">
-            <a href="{{ route($route.'.index') }}" class="btn btn-info">{{ __('dashboard.refresh') }}</a>
-        </div>
+<!-- start page title -->
+@include('admin.inc.breadcrumb')
+<!-- end page title -->
+
+<div class="row mb-3">
+    <div class="col-12">
+        <a href="{{ route($route.'.index') }}" class="btn btn-info">{{ __('dashboard.refresh') }}</a>
     </div>
+</div>
 
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="header-title">{{ $title }} {{ __('dashboard.list') }}</h4>
+            </div>
+            <div class="card-body">
 
-    <div class="row">
-        <div class="col-12">
-
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="header-title">{{ $title }} {{ __('dashboard.list') }}</h4>
-                </div>
-                <div class="card-body">
-
-                  <!-- Data Table Start -->
-                  <div class="table-responsive">
+                <!-- Data Table Start -->
+                <div class="table-responsive">
                     <table id="basic-datatable" class="table table-striped table-hover table-dark nowrap full-width">
                         <thead>
+                            <tr>
+                                <th>SL</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>City</th>
+                                <th>Time</th>
+                                <th>Date</th>
+                                <th>Location</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($rows as $key => $row)
+                                @php
+                                    $today = \Carbon\Carbon::today();
+                                    $rowDate = \Carbon\Carbon::parse($row->date);
+                                    $daysDiff = $today->diffInDays($rowDate, false);
+                                @endphp
                                 <tr>
-                                    <th>SL</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Phone</th>
-                                    <th>City</th>
-                                    <th>Time</th>
-                                    <th>Date</th>
-                                    <th>Location</th>
-                                    <th>Status</th>
-                                    <th>Action</th>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>{{ $row->name }}</td>
+                                    <td>{{ $row->email }}</td>
+                                    <td>{{ $row->phone }}</td>
+                                    <td>{{ $row->city }}</td>
+                                    <td>{{ $row->meeting_time }}</td>
+                                    <td style="font-weight: bold; color:
+                                        {{ $daysDiff >= 0 && $daysDiff <= 7 ? 'red' :
+                                           ($daysDiff > 7 && $daysDiff <= 14 ? 'green' : 'inherit') }};">
+                                        {{ $row->date }}
+                                    </td>
+                                    <td>{{ $row->location }}</td>
+                                    <td>
+                                        <input 
+                                            type="checkbox"
+                                            class="status-toggle"
+                                            data-id="{{ $row->id }}"
+                                            data-toggle="toggle"
+                                            data-on="Approved"
+                                            data-off="Pending"
+                                            data-onstyle="success"
+                                            data-offstyle="danger"
+                                            {{ $row->status == 'approve' ? 'checked' : '' }}>
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('admin.meetinggets.show', [$row->id]) }}" class="btn btn-success btn-sm">
+                                            <i class="fas fa-eye"></i>
+                                        </a>
+                                        <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal-{{ $row->id }}">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                        @include('admin.inc.delete')
+                                    </td>
                                 </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($rows as $key => $row)
-                                    @php
-                                        $today = \Carbon\Carbon::today();
-                                        $rowDate = \Carbon\Carbon::parse($row->date);
-                                        $daysDiff = $today->diffInDays($rowDate, false);
-                                    @endphp
-                                    <tr>
-                                        <td>{{ $key + 1 }}</td>
-                                        <td>{{ $row->name }}</td>
-                                        <td>{{ $row->email }}</td>
-                                        <td>{{ $row->phone }}</td>
-                                        <td>{{ $row->city }}</td>
-                                        <td>{{ $row->meeting_time }}</td>
-                                        <td style="font-weight: bold; color:
-                                            {{ $daysDiff >= 0 && $daysDiff <= 7 ? 'red' :
-                                               ($daysDiff > 7 && $daysDiff <= 14 ? 'green' : 'inherit') }};">
-                                            {{ $row->date }}
-                                        </td>
-                                        <td>{{ $row->location }}</td>
-                                        <td>
-                                            <input type="checkbox" class="status-toggle"
-                                                data-id="{{ $row->id }}"
-                                                data-toggle="toggle"
-                                                data-on="Approve"
-                                                data-off="Pending"
-                                                data-onstyle="success"
-                                                data-offstyle="danger"
-                                                {{ $row->status == 'approve' ? 'checked' : '' }}>
-                                        </td>
-                                        <td>
-                                            <a href="{{ route('admin.meetinggets.show', [$row->id]) }}" class="btn btn-success btn-sm">
-                                                <i class="fas fa-eye"></i>
-                                            </a>
-                                            <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal-{{ $row->id }}">
-                                                <i class="fas fa-trash-alt"></i>
-                                            </button>
-                                            @include('admin.inc.delete')
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div> 
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <!-- Data Table End -->
             </div>
         </div>
     </div>
@@ -105,6 +106,8 @@
 
 <script>
 $(document).ready(function () {
+    $('.status-toggle').bootstrapToggle();
+
     $('.status-toggle').change(function () {
         let status = $(this).prop('checked') ? 'approve' : 'pending';
         let id = $(this).data('id');
@@ -121,7 +124,7 @@ $(document).ready(function () {
                 if (res.success) {
                     toastr.success('Status updated to ' + res.status);
                 } else {
-                    toastr.error('Update failed');
+                    toastr.error('Status update failed');
                 }
             },
             error: function () {
