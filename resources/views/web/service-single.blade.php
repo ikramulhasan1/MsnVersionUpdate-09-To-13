6b3be294bc363d921ce389a18a1e2d8be447508a
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-16 02:45:39 +0600

    ggssddddeffdfkfsfdj

--- resources/views/web/service-single.blade.php
+++ resources/views/web/service-single.blade.php
@@ -477,9 +477,9 @@ $header = \App\Models\PageSetup::page('services');
             }
         });
 </script>
+<!-- ✅ Latest jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script><script>
     $(document).ready(function(){
       $('.lazy').slick({
