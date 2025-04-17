05c160b39cb1225888c6585a9b53be3db6a21eb0
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-17 23:47:00 +0600

    pm

--- resources/views/web/service-single.blade.php
+++ resources/views/web/service-single.blade.php
@@ -350,7 +350,7 @@ $header = \App\Models\PageSetup::page('services');
 
 @if(isset($service))
 <!--Sidebar Page Container-->
-<div style="background-color: #f7fff9" class="sidebar-page-container">
+<div style="background-color: #ffffff" class="sidebar-page-container">
     <div class="mx-5">
         <div class="row clearfix mb-5">
             <!--Sidebar Side-->
