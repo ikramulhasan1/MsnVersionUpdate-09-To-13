f1d68b9c2737a6d2ff9998e5ddad2f6fbc0af876
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-17 17:09:32 +0600

    hdsddfdderdffdrrffffdmfdffdffffferfdfddfff

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -275,7 +275,7 @@
         }
         
       </style>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       {{-- google analytics --}}
       <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-FQTTGFBMBE"></script>
@@ -305,8 +305,8 @@
                         <!--Top Left-->
                         <div class="top-left clearfix">
                             <ul class="links clearfix">
-                                @if(isset($setting->email_one))
-                                <li><span class="icon fa-solid fa-envelope-open-text"></span>{{ $setting->email_one }}</li>
+                                @if(isset($setting->email_one))<i class="fa-solid fa-envelope"></i>
+                                <li><span class=" fa-solid fa-envelope"></span>{{ $setting->email_one }}</li>
                                 @endif
                             </ul>
                         </div>
