8ed6035489e849c1719bfa3ab4d5c4cb15c57035
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-17 17:15:25 +0600

    hdsddfdderdffdrrffffdmfdffdffffferfdfddfffefr

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -780,7 +780,7 @@
                                     $page_quote = \App\Models\PageSetup::page('get-quote');
                                     @endphp
                                     @if(isset($page_quote))
-                                    <li class="advisor-box bg-danger {{ Request::is('get-quote*') ? 'current' : '' }}">
+                                    <li style="background-color: red" class="advisor-box px-2 {{ Request::is('get-quote*') ? 'current' : '' }}">
                                         <a class="text-white " href="{{ route('get-quote') }}">{{ $page_quote->title }}</a>
                                     </li>
                                     @endif
