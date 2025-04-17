89b466c0cb707fc478536954f9ca296d540415db
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-17 15:04:49 +0600

    hdsddfdderdffdrrffffdmfdff

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -602,8 +602,8 @@
                 <div class="container clearfix">
                     @if(isset($setting))
                     <!--Logo-->
-                    <div class="logo pull-left">
-                        <a href="{{ route('home') }}" class="img-responsive"><img src="{{ asset('/uploads/setting/'.$setting->logo_path) }}" alt="Logo"></a>
+                    <div class="logo pull-left d-flex align-items-center">
+                        <a href="{{ route('home') }}" class="img-responsive"><img class="w-50" src="{{ asset('/uploads/setting/'.$setting->logo_path) }}" alt="Logo"></a>
                     </div>
                     @endif
 
