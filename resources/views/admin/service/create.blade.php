fc2b4f0e4ef408eaba737e2e80219d4ec4fab0a0
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-18 19:56:16 +0600

    pmffdsffdfffffffdeffefgffgddfefdgfgfegtffeffstqwfrffeeeffrf

--- resources/views/admin/service/create.blade.php
+++ resources/views/admin/service/create.blade.php
@@ -240,7 +240,7 @@ document.addEventListener("DOMContentLoaded", function () {
         group.classList.add('mb-2');
         group.innerHTML = `
             <input type="text" class="form-control mb-1" name="faqs[${faqIndex}][question]" placeholder="${faqIndex}. Question" required>
-            <input type="text" class="form-control mb-1" name="faqs[${faqIndex}][answer]" placeholder="${faqIndex+2-1}. Answer" required>
+            <input type="text" class="form-control mb-1" name="faqs[${faqIndex}][answer]" placeholder="${faqIndex+2-2}. Answer" required>
         `;
         wrapper.appendChild(group);
         faqIndex++;
