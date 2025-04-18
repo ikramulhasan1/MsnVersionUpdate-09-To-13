3dfb2dcf2933652e1dfa27fbb7c9faddc4499bee
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-19 05:12:26 +0600

    ghggfgfffdddfdkdddfchddndnm

--- resources/views/admin/service/edit.blade.php
+++ resources/views/admin/service/edit.blade.php
@@ -149,7 +149,7 @@
                                 {{ $key+1 }}. 
                                 <input type="text" class="form-control mb-1" name="faqs[{{ $key }}][title]" value="{{ $faq->title }}" placeholder="{{ $key+1 }}. Question" required>
                                 <input type="text" class="form-control mb-1" name="faqs[{{ $key }}][description]" value="{{ $faq->description }}" placeholder="{{ $key+1 }}. Answer" required>
-                                <input type="hidden" class="form-control mb-1" name="faqs[{{ $key }}][type]" value="service" required>
+                                <input type="hidden" class="form-control mb-1" name="faqs[{{ $key }}][type]" value="{{ $faq->type }}" required>
                                 <select hidden name="faqs[{{ $key }}][category_id]">
                                     @foreach ($faqCategories as $category)
                                         <option value="{{ 12 }}" @if($category->id == $faq->category_id) selected @endif>{{ $category->name }}</option>
