e7351438bdf6f8dedfdee396dd6d5aed5e3a43ec
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-21 05:26:37 +0600

    processfgggf

--- resources/views/web/service-single.blade.php
+++ resources/views/web/service-single.blade.php
@@ -550,7 +550,7 @@ $header = \App\Models\PageSetup::page('services');
         </div>
         
         {{-- process-section --}}
-        <section class="process-section my-5 mx-5">
+        <section class="process-section my-5 px-5">
           <div class="container">
             <div class="process-section-title">
               <h2>Our Approach to UI UX Design Services</h2>
