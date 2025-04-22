bd584712422fd12500c43bddebcf751d38a9f925
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-22 21:29:35 +0600

    hsehffggfgffffedddfffd

--- resources/views/admin/service/edit.blade.php
+++ resources/views/admin/service/edit.blade.php
@@ -206,7 +206,7 @@
                                 </div> 
                                 <div class="col-11">
                                     <input type="text" class="form-control mb-1" name="industries[{{ $key }}][title]" value="{{ $industry->title }}" placeholder="{{ $key+1 }}. Title" required>                                
-                                    <input type="text" class="form-control mb-1" name="industries[{{ $key }}][link]" value="{{ $industry->link }}" placeholder="{{ $key+1 }}. Link" required>                                
+                                    <input type="text" class="form-control mb-1" name="industries[{{ $key }}][link]" value="{{ $industry->link }}" placeholder="{{ $key+1 }}. Link">                                
                                 </div>
                             </div>
                             @endforeach
@@ -386,8 +386,8 @@ function addIndustry() {
     const industryGroup = document.createElement('div');
     industryGroup.classList.add('form-group', 'industry-group', 'col-10', 'mb-2');
     industryGroup.innerHTML = `
-        <input type="text" class="form-control mb-1" name="industries[${industryIndex}][title]" placeholder="${industryIndex + 1}. Title" required>
-        <input type="text" class="form-control mb-1" name="industries[${industryIndex}][link]" placeholder="${industryIndex + 1}. Link" required>
+        <input type="text" class="form-control mb-1" name="industries[${industryIndex}][title]" placeholder="${industryIndex + 1}. Title">
+        <input type="text" class="form-control mb-1" name="industries[${industryIndex}][link]" placeholder="${industryIndex + 1}. Link">
     `;
 
     // Insert before the last column (button)
