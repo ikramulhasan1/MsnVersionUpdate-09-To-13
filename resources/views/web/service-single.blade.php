b1c1a93bffd7e368d3a84035093f4782b7f47a1a
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-22 19:55:54 +0600

    dsefrfgg

--- resources/views/web/service-single.blade.php
+++ resources/views/web/service-single.blade.php
@@ -494,8 +494,10 @@ $header = \App\Models\PageSetup::page('services');
                     <div class="sidebar-widget sidebar-blog-category">
                         <ul class="blog-cat">
                             @foreach($service_lists as $service_list)
-                            <img src="{{ asset('uploads/industry/tick-inside-circle.png') }}" alt="" srcset="">
+                            <div class="d-f">
+                              <img style="width: 15px; height: 15px;" src="{{ asset('uploads/industry/tick-inside-circle.png') }}" alt="" srcset="">
                             <li class="@if($service_list->id == $service->id) active @endif"><a href="{{ route('service.single', $service_list->slug) }}">{!! str_limit(strip_tags($service_list->short_title), 60, ' ...') !!}</a></li>
+                            </div>
                             @endforeach
                         </ul>
                     </div>
