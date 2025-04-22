c2a9ae22576bb35b5369af0e4c3c8f6c93eb9449
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-23 04:54:57 +0600

    hs9offdd

--- resources/views/admin/service/edit.blade.php
+++ resources/views/admin/service/edit.blade.php
@@ -200,7 +200,7 @@
                         <div class="row industry-row">
                        
                             @foreach ($row->industries as $key => $industry)
-                            <div class="form-group col-10 industry-group mb-2 row" id="industry-{{ $industry->id }}">
+                            <div class="form-group col-10 industry-group mb-2 row align-items-center" id="industry-{{ $industry->id }}">
                                 <div class="col-1">
                                     {{ $key+1 }}.
                                 </div> 
