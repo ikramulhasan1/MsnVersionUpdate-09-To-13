9c5ac89c45955292a5cf1ae128dc09edc29729ed
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-24 22:11:53 +0600

    dfwfrfgdffffffffffstfffedffffgfg

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -138,7 +138,6 @@ class SliderController extends Controller
      */
     public function update(Request $request, Slider $slider)
     {
-        dd(strip_tags($request->description));
         // Field Validation
         $request->validate([
             'title' => 'required|max:191|unique:sliders,title,'.$slider->id,
