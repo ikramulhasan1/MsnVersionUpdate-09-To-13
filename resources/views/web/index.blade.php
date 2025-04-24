74cf27e7aabc2ae722a9e45630bad0643d2bb346
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-24 23:24:40 +0600

    dfwfrfgdffffffffffstfffedffffgfgffdffdffgf

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -196,47 +196,69 @@ $header = \App\Models\PageSetup::page('home');
       </div> --}}
       
       <div class="carousel-wrap">
-  <div class="owl-carousel owl-theme">
-    @foreach($sliders as $slider)
-      <div class="item"
-           style="position: relative; overflow: hidden; {{ $slider->media_type === 'image' ? 'background-image: url('.asset('uploads/slider/'.$slider->image_path).'); background-size: cover; background-position: center;' : '' }}">
-
-        {{-- YOUTUBE BACKGROUND VIDEO --}}
-        @if($slider->media_type === 'video' && $slider->video_id)
-          <div id="player-{{ $loop->index }}" class="youtube-bg-video" data-video-id="{{ $slider->video_id }}"></div>
-        @endif
-
-        <div class="row w-100 position-relative" style="z-index: 2;">
-          <div class="col-md-8 item-content">
-            <div>
-              <h1>{{ $slider->title }}</h1>
-              <p>{!! $slider->description !!}</p>
-
-              @php $page_contact = \App\Models\PageSetup::page('contact-us'); @endphp
-
-              @if(isset($page_contact))
-                <a style="margin-top: 10px; position: relative; top: 150px;" href="{{ route('contact') }}" class="btn">
-                  {{ __('common.contact_us') }}
-                </a>
+        <div class="owl-carousel owl-theme">
+          @foreach($sliders as $slider)
+            @php
+              $style = '';
+              if ($slider->media_type === 'image' && $slider->image_path) {
+                $style = "background-image: url('" . asset('uploads/slider/' . $slider->image_path) . "'); background-size: cover; background-position: center;";
+              }
+            @endphp
+      
+            <div class="item"
+                 style="justify-content: space-around; position: relative; min-height: 100vh; {{ $style }}"
+                 @if($slider->media_type === 'video' && $slider->video_id)
+                   data-video-id="{{ $slider->video_id }}"
+                 @endif>
+      
+              {{-- Background YouTube Video --}}
+              @if($slider->media_type === 'video' && $slider->video_id)
+                <div class="video-embed" style="position: absolute; inset: 0; z-index: 0; overflow: hidden;">
+                    <iframe
+                    width="100%" height="100%"
+                    src="https://www.youtube.com/embed/{{ $slider->video_id }}?autoplay=1&mute=1&loop=1&controls=0&showinfo=0&playlist={{ $slider->video_id }}&modestbranding=1&rel=0"
+                    frameborder="0"
+                    allow="autoplay; encrypted-media"
+                    allowfullscreen
+                    style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; pointer-events: none; z-index: 0;">
+                  </iframe>
+                  
+                </div>
               @endif
-
-              @if($slider->link)
-                <a style="margin-top: 10px; position: relative; top: 150px;" href="{{ $slider->link }}" target="_blank" class="btn">
-                  {{ __('common.services') }}
-                </a>
-              @endif
+      
+              {{-- Foreground Content --}}
+              <div class="row w-100 position-relative" style="z-index: 2;">
+                <div class="col-md-8 item-content">
+                  <div>
+                    <h1>{{ $slider->title }}</h1>
+                    <p>{!! $slider->description !!}</p>
+      
+                    @php
+                      $page_contact = \App\Models\PageSetup::page('contact-us');
+                    @endphp
+      
+                    @if(isset($page_contact))
+                      <a href="{{ route('contact') }}" class="btn" style="margin-top: 10px; position: relative; top: 150px;">
+                        {{ __('common.contact_us') }}
+                      </a>
+                    @endif
+      
+                    @if(isset($slider->link))
+                      <a href="{{ $slider->link }}" class="btn" target="_blank" style="margin-top: 10px; position: relative; top: 150px;">
+                        {{ __('common.services') }}
+                      </a>
+                    @endif
+                  </div>
+                </div>
+      
+                <div class="col-md-4 d-flex align-items-center justify-content-center short-item">
+                  <button class="btn">Discover</button>
+                </div>
+              </div>
             </div>
-          </div>
-
-          <div class="col-md-4 d-flex align-items-center justify-content-center short-item">
-            <button class="btn">Discover</button>
-          </div>
+          @endforeach
         </div>
       </div>
-    @endforeach
-  </div>
-</div>
-
       
 </section>
 <!-- End Bnner Section -->
