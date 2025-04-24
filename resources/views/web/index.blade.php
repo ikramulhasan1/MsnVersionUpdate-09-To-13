484e08f973fc4e6d139aba836697eca426d71666
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-24 20:45:53 +0600

    dfwfrfgdffffffffffs

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -142,7 +142,7 @@ $header = \App\Models\PageSetup::page('home');
                     background-image: url('{{ asset('uploads/slider/'.$slider->image_path) }}');
                     background-size: cover;
                     background-position: center;
-                  
+                    height: 600px;
                     width: 100%;
                   ">
                 </div>
