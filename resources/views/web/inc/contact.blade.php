5f6e0d79ec56fdcb31fb7d6569b90c57db804f48
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-26 20:07:20 +0600

    hgcfdddffdsdfffsfffddsdffffdddfjjifffffsdd

--- resources/views/web/inc/contact.blade.php
+++ resources/views/web/inc/contact.blade.php
@@ -255,7 +255,7 @@
   </div>
 
   <div style="background-image: url('https://t4.ftcdn.net/jpg/10/95/98/59/240_F_1095985933_J2wC9izxs9fZHvvgFxPC7sKutX8ntwhl.jpg');" class="calendar-section">
-    <h2>Select Date</h2>
+    {{-- <h2>Select Date</h2> --}}
     <div class="calendar-wrapper">
       <div id="calendar"></div>
     </div>
