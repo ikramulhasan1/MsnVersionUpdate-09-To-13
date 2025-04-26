bf211318d9503097c81e4cbcfb94d959c8ff9cba
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-26 22:02:35 +0600

    hgcfdddffdsdfffsfffddsdffffdddfjjifffffsddssdddffffffdffffsdssssddd

--- resources/views/web/inc/contact.blade.php
+++ resources/views/web/inc/contact.blade.php
@@ -37,7 +37,7 @@
     }
 
     .form-section {
-      flex: 1 1 50%;
+      flex: 1 1 55%;
       padding: 50px;
       background-color: #F2F6FF;
     }
@@ -56,7 +56,7 @@
   }
   
   .calendar-section {
-      flex: 1 1 50%;
+      flex: 1 1 45%;
       /* background: #fff; */
       display: flex;
       align-items: center;
