43b94d543283d8e9269582d64fee6959152e7f0a
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-27 15:44:56 +0600

    hxsdsdssdmff

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -480,7 +480,7 @@ body {
           <div class="about-glass-card shadow-sm">
             <h3>{{ $about->title }}</h3>
             <div class="about-content">
-              @dd(strip_tags($about->description))
+              @dd($about->description)
               {!! $about->description !!}
             </div>
             
