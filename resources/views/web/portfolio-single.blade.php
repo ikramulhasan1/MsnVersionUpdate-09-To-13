d985969fe40f36661d3228d2bc99d3a73e67c42e
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-27 23:49:02 +0600

    hx34df

--- resources/views/web/portfolio-single.blade.php
+++ resources/views/web/portfolio-single.blade.php
@@ -120,9 +120,39 @@ $header = \App\Models\PageSetup::page('portfolio');
     .description>p {
         font-size: 18px !important;
     }
+
+
+
+    
+        /* Hero Section */
+        .about-hero-section {
+      background: linear-gradient(135deg, rgba(106,17,203,0.9), rgba(37,117,252,0.9)), url('//images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') no-repeat center center/cover;
+      height: 40vh;
+      color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+      position: relative;
+      overflow: hidden;
+    }
+
+    .about-hero-section h1 {
+      font-size: 60px;
+      font-weight: 700;
+      margin-bottom: 20px;
+    }
+
+    .about-hero-section p {
+      font-size: 22px;
+      max-width: 700px;
+      margin: 0 auto;
+      opacity: 0.9;
+    }
+
 </style>
 <!--Page Title-->
-<section class="page-title">
+{{-- <section class="page-title">
     <div class="container">
         <div class="inner-container clearfix">
             <div class="title-box">
@@ -136,7 +166,29 @@ $header = \App\Models\PageSetup::page('portfolio');
             </div>
         </div>
     </div>
-</section>
+</section> --}}
+
+<section class="about-hero-section" data-aos="fade">
+    <div class="container">
+      <h1>{{ $portfolio->title }}</h1>
+      <!-- <p>Building the Future of Technology and Business Innovation Together.</p> -->
+    </div>
+  </section>
+    <section class="page-title p-0" style="background-color: black;">
+        <div class="container d-flex" style="height: 40px; align-items: center; justify-content: flex-end;">
+            <div class="inner-container clearfix">
+                {{-- <div class="title-box">
+                    <h1>{{ __('navbar.contact') }}</h1>
+                </div> --}}
+                <div class="bread-crumb">
+                    <ul class="p-0">
+                        <li>{{ __('navbar.portfolio-detail') }}</li>
+                        <li><a href="{{ route('home') }}">{{ __('navbar.home') }}</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
 <!--End Page Title-->
 
 @if(isset($portfolio))
