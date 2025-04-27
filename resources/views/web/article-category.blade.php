a26bbaa75ef6f17f0326259a41506a3330b81191
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-28 00:45:00 +0600

    hx34dfdddded

--- resources/views/web/article-category.blade.php
+++ resources/views/web/article-category.blade.php
@@ -288,7 +288,7 @@ $header = \App\Models\PageSetup::page('blog');
       <a href="{{ route('blog.single', $articles[0]->slug) }}" class="read-more">READ MORE</a>
     </div>
     <div class="col-md-6">
-      <img src="{{ asset('uploads/article/'.$articles[0]->image_path) }}" alt="{{ $articles->title }}" class="img-fluid rounded">
+      <img src="{{ asset('uploads/article/'.$articles[0]->image_path) }}" alt="{{ $articles[0]->title }}" class="img-fluid rounded">
     </div>
   </div>
 </div>
