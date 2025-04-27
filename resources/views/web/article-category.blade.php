f46e0a287fd9c307a55b466bb65b46ae4b5d8d08
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-28 03:31:29 +0600

    h3f

--- resources/views/web/article-category.blade.php
+++ resources/views/web/article-category.blade.php
@@ -140,9 +140,12 @@ use Illuminate\Support\Str;
 <!-- Search Bar -->
 <div class="container search-bar">
     <div class="row">
-        <div class="col-12 d-flex justify-content-end">
-            <input type="text" class="form-control w-25" placeholder="Search" disabled>
-        </div>
+        <form method="get" action="{{ route('blog.search') }}">
+            <div class="col-12 d-flex justify-content-end">
+                <input name="search" value="@if(isset($search)){{ $search }}@endif" type="text" class="form-control w-25" placeholder="Search">
+                <button type="submit"><span class="icon fa fa-search"></span></button>
+            </div>
+        </form>
     </div>
 </div>
 
@@ -183,8 +186,6 @@ use Illuminate\Support\Str;
     </div>
 </div>
 
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- Blog Loading Script -->
 <script>
