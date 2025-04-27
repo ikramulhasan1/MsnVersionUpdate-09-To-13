4ffc29b0d995b8b495979fb2de672d037e64aed0
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-27 20:12:57 +0600

    hxsdsdssdmfffddfdfsfdddfddddsdfdffssdsfddsddsdfdfdffffd

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -75,10 +75,10 @@
     <!-- Hero Section -->
   <section class="about-hero-section" data-aos="fade">
     <div class="container d-flex" style="height: 40px; align-items: center; justify-content: flex-end;">
-      <h1>Contact Us</h1>
+      <h1 style="text-align: center">Contact Us</h1>
       <!-- <p>Building the Future of Technology and Business Innovation Together.</p> -->
     </div>
-  </section>
+  </section style="background-color: black;">
     <section class="page-title p-0">
         <div class="container">
             <div class="inner-container clearfix">
