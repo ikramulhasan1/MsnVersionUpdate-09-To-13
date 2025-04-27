540f7f2a43a36a46cd7bab56aec104bd277b6a11
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-27 21:58:28 +0600

    hxsdsdssdmfffddfdfsfdddfddddsdfdffssdsfddsddsdfdfdffffdfsdfsdffffffsf

--- resources/views/web/faqs.blade.php
+++ resources/views/web/faqs.blade.php
@@ -163,6 +163,33 @@ $header = \App\Models\PageSetup::page('faqs');
       margin: 0 auto;
       opacity: 0.9;
     }
+
+
+
+    /*  */
+    /* client */
+    .partner-section {
+      padding: 60px 0;
+      background-color: #F5F7F8;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+
+    .partner-section h2 {
+      font-weight: 700;
+      text-align: center;
+      margin-bottom: 40px;
+      color: #333333;
+    }
+
+    /* Custom 5-column layout for large screens */
+    @media (min-width: 992px) {
+      .col-lg-2-4 {
+        flex: 0 0 auto;
+        width: 19%;
+      }
+    }
+    
+
 </style>
 <!--Page Title-->
 
@@ -249,7 +276,7 @@ $section_clients = \App\Models\Section::section('clients');
 @endphp
 @if(count($clients) > 0 && isset($section_clients))
 <!--Clients Section-->
-<section class="clients-section style-two">
+{{-- <section class="clients-section style-two">
     <div class="container">
         <div class="sponsors-outer">
             <!--Sponsors Carousel-->
@@ -261,6 +288,18 @@ $section_clients = \App\Models\Section::section('clients');
                 @endforeach
             </ul>
         </div>
+    </div>
+</section> --}}
+<section class="partner-section">
+    <div class="container">
+    <h2>Enterprises & Tech Companies Worldwide Trust Us</h2>
+    <div class="row gap-2 justify-content-center text-center partner-logos align-items-center">
+        @foreach($clients as $client)
+        <div class="col-6 col-sm-4 col-md-2 col-lg-2-4 bg-white px-3 py-0 d-flex align-items-center justify-content-center m-1" style="height: 90px;">
+        <img src="{{ asset('uploads/client/'.$client->image_path) }}" alt="{{ $client->title }}" class="img-fluid my-1"/>
+        </div>
+        @endforeach
+    </div>
     </div>
 </section>
 <!--End Clients Section-->
