314a730304022c9f3a7f935d18aea79bd990d385
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-27 20:17:05 +0600

    hxsdsdssdmfffddfdfsfdddfddddsdfdffssdsfddsddsdfdfdffffdfs

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -73,14 +73,14 @@
 
     <!--Page Title-->
     <!-- Hero Section -->
-  <section class="about-hero-section" style="background-color: black;" data-aos="fade">
-    <div class="container d-flex" style="height: 40px; align-items: center; justify-content: flex-end;">
+  <section class="about-hero-section" data-aos="fade">
+    <div class="container d-flex">
       <h1 style="text-align: center;">Contact Us</h1>
       <!-- <p>Building the Future of Technology and Business Innovation Together.</p> -->
     </div>
   </section>
-    <section class="page-title p-0">
-        <div class="container">
+    <section class="page-title p-0" style="background-color: black;">
+        <div class="container" style="height: 40px; align-items: center; justify-content: flex-end;">
             <div class="inner-container clearfix">
                 {{-- <div class="title-box">
                     <h1>{{ __('navbar.contact') }}</h1>
