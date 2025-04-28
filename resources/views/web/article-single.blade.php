da8770dfc09f7de3294f7d9fd322b1e7f07bc035
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-29 02:22:44 +0600

    h9dfffdfdddf

--- resources/views/web/article-single.blade.php
+++ resources/views/web/article-single.blade.php
@@ -1084,15 +1084,7 @@ color: #222222;
               
               <div>
                 @include('web.layouts.googlemeet')
-                <div>
-                  <button id="open-modal" class="button google-meet-button" style="background-color: #48bb78; color: white; padding: 12px 24px; cursor: pointer; display: flex; align-items: center;">
-                    <div class="logo-container">
-                      <img id="google-meet-img" src="https://www.gstatic.com/meet/google_meet_horizontal_wordmark_2020q4_2x_icon_124_40_292e71bcb52a56e2a9005164118f183b.png" alt="Google Meet Logo" />
-                      <img id="zoom-img" src="https://upload.wikimedia.org/wikipedia/commons/7/7b/Zoom_Communications_Logo.svg" alt="Zoom Logo" />
-                    </div>
-                    <span style="font-weight: 600; font-size: 18px; color: white; margin-left: 12px;">Book a Meeting</span>
-                  </button>
-                </div>
+                
               </div>
             </div>
           </div>
