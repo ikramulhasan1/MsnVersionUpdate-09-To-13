d34b5a52516c71376245ec7a8b9cd4ef39f148d7
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-29 03:40:33 +0600

    h9dfffdfdddfffffddjdf

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -368,6 +368,8 @@
 
         gtag('config', 'G-FQTTGFBMBE');
     </script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+
 </head>
 
 <body>
