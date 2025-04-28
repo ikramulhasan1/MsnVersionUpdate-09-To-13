fc20c7e431b8d3d5f657b20b66db4729d28eb7b0
ikramulhasan1 <hasanikramul926@gmail.com>
2025-04-29 03:54:14 +0600

    h9dfffdfdddfffffddjdfkdfd

--- resources/views/web/layouts/master.blade.php
+++ resources/views/web/layouts/master.blade.php
@@ -1332,7 +1332,7 @@
                                 <a href="skype:{{ $social->skype }}?chat" target="_blank"><i class="bi bi-skype"></i></a>
                             @endif
                             @if(isset($social->whatsapp))
-                                <a rel="noopener noreferrer" class="whatsapp" href="https://wa.me/{{ str_replace(' ', '', $social->whatsapp) }}" target="_blank"><i class="bi bi-whatsapp"></i></a>
+                                <a rel="noopener noreferrer" class="whatsapp d-flex justify-content-center align-items-center" href="https://wa.me/{{ str_replace(' ', '', $social->whatsapp) }}" target="_blank"><i class="bi bi-whatsapp"></i></a>
                             @endif
                             
                         </div>
