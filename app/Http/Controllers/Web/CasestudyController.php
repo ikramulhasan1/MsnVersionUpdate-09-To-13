d1986585c6c64bd108e95db60e4abbf91c0ab88d
ikramulhasan1 <hasanikramul926@gmail.com>
2025-05-02 18:05:13 +0600

    gf5d

--- app/Http/Controllers/Web/CasestudyController.php
+++ app/Http/Controllers/Web/CasestudyController.php
@@ -1,33 +1,36 @@
 <?php
 
-namespace App\Http\Controllers\Web;
-use Illuminate\Http\Request;
+namespace App\Http\Controllers\web;
 
+use App\Models\Portfolio;
+use Illuminate\Http\Request;
+use App\Models\PortfolioCategory;
 use App\Http\Controllers\Controller;
 
 class CasestudyController extends Controller
 {
-    public function index()
+     public function index()
     {
         // Portfolio Categories                                
-        // $data['portfolio_categories'] = PortfolioCategory::where('status', '1')
-        //     ->orderBy('id', 'asc')
-        //     ->get();
+        $data['portfolio_categories'] = PortfolioCategory::where('status', '1')
+            ->orderBy('id', 'asc')
+            ->get();
 
-        // // Portfolios                                
-        // $data['portfolios'] = Portfolio::where('status', '1')
-        //     ->orderBy('id', 'desc')
-        //     ->get();
+        // Portfolios                                
+        $data['portfolios'] = Portfolio::where('status', '1')
+            ->orderBy('id', 'desc')
+            ->get();
 
-        return view('web.portfolios');
+        return view('web.case-studies', $data);
     }
 
     public function show($slug)
     {
-        // Portfolio                                ['portfolio'] = Portfolio::where('slug', $slug)
-        //     ->where('status', '1')
-        //     ->firstOrFail();
+        // Portfolio                                
+        $data['portfolio'] = Portfolio::where('slug', $slug)
+            ->where('status', '1')
+            ->firstOrFail();
 
-        return view('web.portfolio-single');
+        return view('web.case-study', $data);
     }
 }
