d8f1574535a7bf32be2be2a1c3be1ba0f3d19fd0
ikramulhasan1 <hasanikramul926@gmail.com>
2025-05-04 02:15:31 +0600

    ghfffsdffkfddffdddfff

--- resources/views/admin/case-study/create.blade.php
+++ resources/views/admin/case-study/create.blade.php
@@ -148,14 +148,24 @@
                         <div class="row process-row">
                             <div class="form-group col-9 faq-group mb-2">
                                 {{-- <input type="text" class="form-control mb-1" name="case[0][case_title]" placeholder="Title"> --}}
-                                <select name="case[0][case_title]" class="form-select" aria-label="Default select example">
+                                {{-- <select name="case[0][case_title]" class="form-select" aria-label="Default select example">
                                     <option selected value="The Challenges">The Challenges</option>
                                     <option value="Solutions We Offered">Solutions We Offered</option>
                                     <option value="Results">Results</option>
                                     <option value="Key Features Delivered">Key Features Delivered</option>
                                     <option value="Client Testimonial">Client Testimonial (if available)</option>
-                                  </select>
-                                  
+                                  </select> --}}
+
+                                  <div class="form-group col">
+                                    <label for="status">{{ __('dashboard.title') }}</label>
+                                    <select class="wide" name="case[0][case_title]" id="status" data-plugin="customselect">
+                                        <option selected value="The Challenges">The Challenges</option>
+                                        <option value="Solutions We Offered">Solutions We Offered</option>
+                                        <option value="Results">Results</option>
+                                        <option value="Key Features Delivered">Key Features Delivered</option>
+                                        <option value="Client Testimonial">Client Testimonial (if available)</option>
+                                    </select>
+                                </div>
                                 <textarea type="text" class="form-control mb-1" name="case[0][case_description]" placeholder="Description"></textarea>
                                 <input type="file" class="form-control mb-1" name="case[0][case_image]">
                                
