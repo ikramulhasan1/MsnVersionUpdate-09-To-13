409d350177d94d03570e78987c966c8d160359ac
ikramulhasan1 <hasanikramul926@gmail.com>
2025-05-05 03:44:18 +0600

    reltionsadddddddsdddydsdfsfdsdfdfffdffdffddd

--- resources/views/web/get-quote.blade.php
+++ resources/views/web/get-quote.blade.php
@@ -227,7 +227,7 @@ $header = \App\Models\PageSetup::page('get-quote');
 
     .quoteFormSection {
       font-family: 'Inter', sans-serif;
-      background: linear-gradient(120deg, #05264A, #052b4a #05154a, #0e054a);
+      background: linear-gradient(120deg, #05264A, #052b4a, #05154a, #0e054a);
       min-height: 100vh;
       display: flex;
       align-items: center;
