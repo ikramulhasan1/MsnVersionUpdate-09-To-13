b3c5d2501723b103cc610fb9850c683e77fd9f8b
ikramulhasan1 <hasanikramul926@gmail.com>
2025-09-11 12:38:38 +0600

    ytr5gggffgfddffffdfgfgfff

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1468,7 +1468,7 @@
 
         </div>
         <div class="d-flex justify-content-center">
-          <a href="{{ route('services') }}" class="btn text-white fw-800"
+          <a href="{{ route('services') }}" class=" text-white fw-800"
             style="background-color: #FF6F2C !important; font-weight: bold;">View All Services</a>
         </div>
       </div>
