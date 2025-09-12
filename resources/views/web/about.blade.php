5f99fdd842abb9d82bb92f5738552ba6787248b9
ikramulhasan1 <hasanikramul926@gmail.com>
2025-09-12 20:27:44 +0600

    ytr5gffdgfgffgdfgdfdfdgffrdfkfgdf

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -472,7 +472,7 @@ color: #009830;
       <!-- <p>Building the Future of Technology and Business Innovation Together.</p> -->
     </div>
   </section>
-  <section class="page-title p-0" style="background-color: #052C58;">
+  <section class="page-title p-0" style="background-color: black;">
     <div class="container d-flex" style="height: 40px; align-items: center; justify-content: flex-end;">
         <div class="inner-container clearfix">
             {{-- <div class="title-box">
