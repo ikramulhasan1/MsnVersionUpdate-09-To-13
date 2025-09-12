3df536a0de78665d89de5c6810398a763fc53ba6
ikramulhasan1 <hasanikramul926@gmail.com>
2025-09-12 11:05:41 +0600

    ytr5gggffgfddffffdfgfgfffgffgddgegfrefgfgfggfyggggetfdgggggggffgdffgd

--- resources/views/web/inc/client.blade.php
+++ resources/views/web/inc/client.blade.php
@@ -19,6 +19,30 @@
         /* margin: auto; */
     }
 
+    .swiper-slide {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        max-height: 80px !important;
+    }
+
+    .swiper-slide img {
+        width: 140px  !important;
+        max-height: 80px !important;
+        margin: 0px !important;
+        object-fit: contain;
+        background: white;
+        /* padding: 10px; */
+        border-radius: 2px;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+        filter: grayscale(100%);
+    transition: all 0.3s ease; /* normal */
+    }
+
+
+
+
+
 .swiper-slide img {
     filter: grayscale(100%);
     transition: all 0.3s ease; /* normal */
@@ -30,6 +54,9 @@
     box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
     transition: all 0.1s ease-in; /* instant feel */
 }
+
+
+
 
     /* .swiper-slide img:hover {
             filter: grayscale(0%);
