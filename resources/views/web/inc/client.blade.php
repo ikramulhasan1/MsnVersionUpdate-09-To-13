11c696a5cabdaa08125702b199174e932503c20e
ikramulhasan1 <hasanikramul926@gmail.com>
2025-09-12 07:29:12 +0600

    ytr5gggffgfddffffdfgfgfffgffgddg

--- resources/views/web/inc/client.blade.php
+++ resources/views/web/inc/client.blade.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Client Logo Carousel</title>
 
     <!-- Swiper CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
@@ -78,11 +71,9 @@
             transition: all 0.3s ease;
         }
     </style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous" />
-</head>
+    
 
-<body>
+
     <section class="client-logo-section">
         <h2>Enterprises & Tech Companies Worldwide Trust Us</h2>
         <div class="row">
@@ -187,9 +178,3 @@
             },
         });
     </script>
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-    crossorigin="anonymous"></script>
-
-</html>
