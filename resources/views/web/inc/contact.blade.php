a0149f61926231b253499344adb30fd81c37af04
ikramulhasan1 <hasanikramul926@gmail.com>
2025-09-14 15:26:22 +0600

    ui*6gggffgffdftdfffregede

--- resources/views/web/inc/contact.blade.php
+++ resources/views/web/inc/contact.blade.php
@@ -214,7 +214,9 @@
 
 
     }
-
+      .form-section>.phone {
+        width: 100% !important;
+      }
     @media (min-width: 576px) {
       .form-section {
         flex: 1 1 55%;
@@ -266,10 +268,9 @@
       width: 100%;
     }
 
-    #phone {
+    /* #phone {
       width: 420px;
-      /* margin-bottom: 20px !important; */
-    }
+    } */
 
     #email {
       margin-top: 20px !important;
@@ -290,7 +291,7 @@
 
       <form id="booking-form">
         <input type="text" id="name" name="name" class="form-control" placeholder="Your Name" required />
-        <input type="tel" id="phone" name="phone" class="form-control" placeholder="Phone Number" required />
+        <input type="tel" id="phone" name="phone" class="form-control phone" placeholder="Phone Number" required />
         <input type="email" id="email" name="email" class="form-control" placeholder="Email Address" required />
         <input type="text" id="location" name="location" class="form-control" placeholder="Location" autocomplete="off"
           required />
