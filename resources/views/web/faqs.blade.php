e2921eb2fefa86c56b16979234b44f22cebbac68
ikramulhasan1 <hasanikramul926@gmail.com>
2025-09-14 14:42:30 +0600

    ui*6gggff

--- resources/views/web/faqs.blade.php
+++ resources/views/web/faqs.blade.php
@@ -184,7 +184,7 @@ $header = \App\Models\PageSetup::page('faqs');
     @media (min-width: 576px) { 
         .partner-section h2 {
             font-weight: 700;
-            font-size: 20px;
+            font-size: 24px;
             text-align: center;
             margin-bottom: 40px;
             color: #333333;
