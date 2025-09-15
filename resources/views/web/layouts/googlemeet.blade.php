f7b1f316e676cfe1c48216c25000cbffc6ebd47c
ikramulhasan1 <hasanikramul926@gmail.com>
2025-09-15 23:19:53 +0600

    all css and js file compressedgfffgfffffdfgffffvgdffsd

--- resources/views/web/layouts/googlemeet.blade.php
+++ resources/views/web/layouts/googlemeet.blade.php
@@ -154,14 +154,7 @@
 </div>
 
 <!-- Scripts -->
-{{-- <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script> --}}
-<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.js"></script>
-<script>
-  flatpickr("#datepicker", {});
-</script>
-
-
-
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
