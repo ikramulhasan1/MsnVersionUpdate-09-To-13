b78c535984f11145217acbef6a38f21384c7d170
ikramulhasan1 <hasanikramul926@gmail.com>
2025-09-16 13:32:20 +0600

    ui8yu7fgfffeddfff

--- resources/views/admin/case-study/edit.blade.php
+++ resources/views/admin/case-study/edit.blade.php
@@ -141,7 +141,7 @@
                             </div>
                         </div>
                         <hr>
-                       @dd($row->faqCategories)
+                       @dd($row)
                         <h3>FAQs</h3>
                         <div class="row faq-row">
                        
