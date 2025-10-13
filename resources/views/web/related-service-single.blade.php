e74406532817c192ce3476301dedb5382f47f8ba
ikramulhasan1 <hasanikramul926@gmail.com>
2025-10-13 22:24:41 +0600

    ju7y6sdsdsddsdeddfddddfddddsdfddddsddeswwsdded

--- resources/views/web/related-service-single.blade.php
+++ resources/views/web/related-service-single.blade.php
@@ -1160,8 +1160,8 @@
 
 
     <!-- JS -->
-  {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script> --}}
-  {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script> --}}
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> --}}
   <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
   <script>
