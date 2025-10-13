bb2d12c31d68b064c13e6ff8374f12da0ce34268
ikramulhasan1 <hasanikramul926@gmail.com>
2025-10-13 22:26:33 +0600

    ju7y6sdsdsddsdeddfddddfddddsdfddddsddeswwsddedd

--- resources/views/web/related-service-single.blade.php
+++ resources/views/web/related-service-single.blade.php
@@ -145,6 +145,11 @@
 {{-- schema section --}}
 
 @section('content')
+<!-- FontAwesome -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+  <!-- intl-tel-input -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.min.css"/>
+  
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
@@ -460,6 +465,8 @@
         margin: 20px auto;
       }
     }
+
+    
     </style>
 
 
@@ -1160,8 +1167,8 @@
 
 
     <!-- JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  {{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script> --}}
+  {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script> --}}
   {{-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> --}}
   <script src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"></script>
   <script>
