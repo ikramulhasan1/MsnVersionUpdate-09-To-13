20d8d035a8345d4525e6c6ad97578dd7fa2de6c5
ikramulhasan1 <hasanikramul926@gmail.com>
2025-10-13 18:39:27 +0600

    ju7y6sd

--- resources/views/web/related-service-single.blade.php
+++ resources/views/web/related-service-single.blade.php
@@ -146,7 +146,7 @@ $header = \App\Models\PageSetup::page('related-service');
 
 @section('content')
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
   <style>
     body {
       font-family: 'Poppins', sans-serif;
@@ -673,7 +673,7 @@ $header = \App\Models\PageSetup::page('related-service');
     </div>
   </section>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
 @endsection
