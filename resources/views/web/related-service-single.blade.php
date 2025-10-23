a22909b93943f4e08aa4aefe66b5d1aaf8ebe045
ikramulhasan1 <hasanikramul926@gmail.com>
2025-10-23 18:53:30 +0600

    hyt98gddffffeddfddeegffdfdgsdgfeefeeff

--- resources/views/web/related-service-single.blade.php
+++ resources/views/web/related-service-single.blade.php
@@ -672,7 +672,7 @@
                             <img src="{{ asset('uploads/portfolio/' . $portfolio->image_path) }}"
                                 class="img-fluid" alt="{{ $portfolio->title }}">
                             <div class="project-overlay">
-                                <a href="{{ route('portfolio.single', $portfolio->slug) }}" class="text-white font-weight-bold">{{ $portfolio->title }}</a>
+                                <h5><a href="{{ route('portfolio.single', $portfolio->slug) }}" class="text-white font-weight-bold">{{ $portfolio->title }}</a></h5>
                             </div>
                         </div>
                     </div>
