434c62ea2539125dd3cc6988c278681b0cd18df7
ikramulhasan1 <hasanikramul926@gmail.com>
2025-10-31 16:25:39 +0600

    hju67ddddddsdeddedddeffdfd

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1765,7 +1765,7 @@
       <div class="case-studies-badge">Case Studies</div>
 
       <div id="case-owl-carousel" class="owl-carousel owl-theme">
-
+@dd($case_studies)
         <!-- Slide 1 -->
         @foreach ($case_studies as $case_study)
         <div class="case-studies-item"
