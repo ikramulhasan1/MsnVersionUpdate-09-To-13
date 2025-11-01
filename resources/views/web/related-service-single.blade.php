42d5d2e303ed325a5427745496d68767f568d037
ikramulhasan1 <hasanikramul926@gmail.com>
2025-11-01 17:15:11 +0600

    kj8kifdds

--- resources/views/web/related-service-single.blade.php
+++ resources/views/web/related-service-single.blade.php
@@ -184,7 +184,7 @@
             left: 0;
             width: 100%;
             height: 100%;
-            background: rgba(0, 0, 0, 0.733);
+            background: rgba(0, 0, 0, 0.829);
         }
 
         .hero-content {
