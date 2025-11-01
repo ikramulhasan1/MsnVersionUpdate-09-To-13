e88e882a95be9b7cf3668285f23f08a62bd664e5
ikramulhasan1 <hasanikramul926@gmail.com>
2025-11-01 17:16:22 +0600

    kj8kifddssd

--- resources/views/web/related-service-single.blade.php
+++ resources/views/web/related-service-single.blade.php
@@ -184,7 +184,7 @@
             left: 0;
             width: 100%;
             height: 100%;
-            background: rgb(0, 0, 0);
+            background: rgba(0, 0, 0, 0.911);
         }
 
         .hero-content {
