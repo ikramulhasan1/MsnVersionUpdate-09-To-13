1ad8ecf583887cfdf58ab9827d3dfe3c82d2b173
ikramulhasan1 <hasanikramul926@gmail.com>
2025-11-02 18:17:24 +0600

    kljm,d

--- resources/views/web/portfolio-single.blade.php
+++ resources/views/web/portfolio-single.blade.php
@@ -137,7 +137,7 @@
           <p class="text-muted">
             {!! $portfolio->description !!}
           </p>
-          <a href="{{ $portfolio->link }}" class="btn btn-primary">View All Projects</a>
+          <a href="{{ $portfolio->link }}" class="btn btn-primary my-3">View All Projects</a>
         </div>
       </div>
     </div>
