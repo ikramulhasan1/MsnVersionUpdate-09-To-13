d093a7fcdb3f62fb6ae8d0681b72b6fd80a5f911
ikramulhasan1 <hasanikramul926@gmail.com>
2026-07-19 00:11:05 +0600

    about-redesign-2026sssdsaskdsessddse

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -220,8 +220,8 @@
 .svc-service-desc ul li::before {
     content: "•";
     position: absolute;
-    left: 10;
-    color: #ff0800;
+    left: 0;
+    color: #ff0000;
     font-weight: bold;
 }
 </style>
