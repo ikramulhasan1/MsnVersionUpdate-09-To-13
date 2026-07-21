d24b104438f3888a8bfa104e3b4b41fa0a227035
ikramulhasan1 <hasanikramul926@gmail.com>
2026-07-21 17:42:57 +0600

    jku7guarawswadswwssew

--- app/Models/Meeting.php
+++ app/Models/Meeting.php
@@ -2,26 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Meeting extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'email',
-        'phone',
-        'location',
-        'latitude',
-        'longitude',
-        'meeting_time',
-        'date',
-        'city',
-        'ip',
-        'distance_km',
-        'distance_time' 
-    ];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'phone',
+    //     'location',
+    //     'latitude',
+    //     'longitude',
+    //     'meeting_time',
+    //     'date',
+    //     'city',
+    //     'ip',
+    //     'distance_km',
+    //     'distance_time',
+    // ];
+
+    protected $guarded = [];
 }
- 
