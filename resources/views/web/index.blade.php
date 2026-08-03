789b3db13f149066925450a9d67f21bff1f942c6
ikramulhasan1 <hasanikramul926@gmail.com>
2026-08-03 12:15:24 +0600

    jkhyu7

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -278,8 +278,7 @@
         <section class="idx-trust-section">
             <div class="container">
                 <p class="idx-trust-text mb-0">
-                    <span class="idx-highlight">Trusted by Customers across 40+ countries to take products from concept
-                        to scale.</span>
+                    <span class="idx-highlight">Helping Businesses Build, Launch & Scale Worldwide.</span>
 
                 </p>
 
