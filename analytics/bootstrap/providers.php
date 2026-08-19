bfb28215fd375a31cec7c9b34633df2dba379698
ikramulhasan1 <hasanikramul926@gmail.com>
2026-08-19 15:31:07 +0600

    kj99eldsdhsdl

--- analytics/bootstrap/providers.php
+++ analytics/bootstrap/providers.php
@@ -3,9 +3,11 @@
 use App\Providers\AppServiceProvider;
 use App\Providers\AuditServiceProvider;
 use App\Providers\DiscoveryServiceProvider;
+use Laravel\Socialite\SocialiteServiceProvider;
 
 return [
     AppServiceProvider::class,
     AuditServiceProvider::class,
     DiscoveryServiceProvider::class,
+    SocialiteServiceProvider::class,
 ];
